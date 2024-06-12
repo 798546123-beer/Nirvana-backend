6783fff681205096f54598c6423fc962b316b231
Zoey <2356413947@qq.com>
2024-06-12 12:04:35 +0800

    remark实体

---
+++ src/main/java/com/example/demo/Entity/Remark.java
@@ -0,0 +1,36 @@
+package com.example.demo.Entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: zoey
+ * \\_/__/
+ * @Date: 2024/06/12/11:50
+ * @Description:
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName(value = "remarks")
+public class Remark {
+    @TableId(value = "remarkId",type = IdType.AUTO)
+    private String remarkId;
+    @TableField(value = "courseId")
+    private String courseId;
+    @TableField(value = "remarkTime")
+    private String remarkTime;
+    @TableField(value = "userId")
+    private String userId;
+    @TableField(value = "content")
+    private String content;
+    @TableField(value = "likes")
+    private Integer likes;
+}
